ebe7a3ab5aa34722d739251232f6ddb5dfbd2d41
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-19 03:50:48 -0300

    code applyed

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main
+        icon="home"
+        title="Home"
+        subtitle="Project from React Chapter">
+        <div className='display-4'>Welcome</div>
+        <hr />
+        <p className="mb-0">Dashboard to exemplify the process to register some data</p>
+    </Main>
---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Made with <i className="fa fa-heart text-danger"></i> by <a href="https://www.thecodelovers.com.br" targe="_new"><strong> The Code Lovers</strong></a>
+        </span>
+    </footer>
